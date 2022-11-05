a950d7f74a446bb5f9fe5ad13e9871023e0c4e80
Nuzulitastwn <63113078+Nuzulitastwn@users.noreply.github.com>
2022-11-05 09:24:37 +0700

    Update Index.js

--- src/new-page/Index.js
+++ src/new-page/Index.js
@@ -7,15 +7,3 @@ export default function New() {
   useEffect(() => {
     count.current = count.current + 1;
   });
-
-  return (
-    <>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <h1>Render Count: {count.current}</h1>
-    </>
-  );
-}
